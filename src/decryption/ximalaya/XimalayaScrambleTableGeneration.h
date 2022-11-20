2bd29b08a7d609a274de3b91296a7dadfe0f80e6
Jixun Wu <jixun.moe@gmail.com>
2022-11-20 01:52:19 +0000

    feat: add xmly table generation API.

---
+++ src/decryption/ximalaya/XimalayaScrambleTableGeneration.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+
+namespace parakeet_crypto::decryption::ximalaya {
+
+std::vector<uint16_t> generate_ximalaya_scramble_table(double mul_init, double mul_step, size_t n);
+
+}  // namespace parakeet_crypto::decryption::ximalaya
